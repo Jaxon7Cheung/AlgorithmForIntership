5b4c741febf27f44fb7d552878aed67dc795d566
Jaxon7Cheung <2104526824@qq.com>
2024-10-23 23:56:30 +0800

    更新常规动规

--- Leetcode/62.不同路径.cpp
+++ Leetcode/62.不同路径.cpp
@@ -14,10 +14,10 @@ public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, 0));
 
-        dp[0][0] = 1;
+        // dp[0][0] = 1;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (!i && !j) continue;
+                // if (!i && !j) continue;
                 if (i && j) {
                     dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
                 } else {
