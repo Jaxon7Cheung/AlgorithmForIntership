0a0e0b18b461127fe04063dd379040bc9d974062
Jaxon7Cheung <2104526824@qq.com>
2024-10-23 23:31:59 +0800

    正则表达式匹配

---
+++ Leetcode/10.正则表达式匹配.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  lt&al
+//
+//  Created by 张旭洋 on 2024/10/7.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size() + 1, n = p.size() + 1;
+        vector<vector<bool>> dp(m, vector<bool>(n, false));
+        dp[0][0] = true;
+        for(int j = 2; j < n; j += 2)
+            dp[0][j] = dp[0][j - 2] && p[j - 1] == '*';
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = p[j - 1] == '*' ?
+                    dp[i][j - 2] || dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'):
+                    dp[i - 1][j - 1] && (p[j - 1] == '.' || s[i - 1] == p[j - 1]);
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+ 
+int main(void) {
+    string s = "ab";
+    string p = ".*";
+    
+    Solution solution;
+    cout << solution.isMatch(s, p) << endl;
+    
+
+    return 0;
+}
