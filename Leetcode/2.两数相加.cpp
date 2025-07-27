c9882204d3323e837826bd984c00c381c8cc25b8
Jaxon7Cheung <2104526824@qq.com>
2025-07-27 16:13:52 +0800

    关键是逆序+位数不齐0补全

---
+++ Leetcode/2.两数相加.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=cpp
+ * @lcpr version=30202
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode();
+        ListNode* p = dummy;
+
+        int carry = 0;
+        int sum = 0;
+        while (l1 || l2) {
+            int x = l1 ? l1->val : 0;
+            int y = l2 ? l2->val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+
+            p->next = new ListNode(sum % 10);
+            p = p->next;
+            
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+        if (carry > 0) {
+            p->next = new ListNode(carry);
+        }
+
+        return dummy->next;
+
+
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [2,4,3]\n[5,6,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0]\n[0]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [9,9,9,9,9,9,9]\n[9,9,9,9]\n
+// @lcpr case=end
+
+ */
+
